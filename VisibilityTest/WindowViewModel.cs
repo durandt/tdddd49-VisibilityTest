b8fae1b924e257f6cbe1d947bbbf15519a3bdbfd
Thibault Durand <thibault.durand@liu.se>
2023-11-29 14:15:37 +0100

    Example of WPF app with Button which visibility is binded to ViewModel

---
+++ VisibilityTest/WindowViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace VisibilityTest;
+
+internal class WindowViewModel : INotifyPropertyChanged
+{
+    public WindowViewModel()
+    {
+        ToggleButtonCommand = new ToggleVisibilityCommand(this);
+        OpenWindowCommand = new OpenWindowCommand();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private Visibility _buttonVisibility = Visibility.Hidden;
+
+    public Visibility ButtonVisibility
+    {
+        get
+        {
+            return _buttonVisibility;
+        }
+        set
+        {
+            _buttonVisibility = value;
+            OnPropertyChanged("ButtonVisibility");
+        }
+    }
+
+    public ICommand ToggleButtonCommand { get; }
+    public ICommand OpenWindowCommand { get; }
+
+    protected virtual void OnPropertyChanged(/*[CallerMemberName]*/ string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+internal class ToggleVisibilityCommand : ICommand
+{
+    private readonly WindowViewModel _viewModel;
+
+    public event EventHandler? CanExecuteChanged;
+
+    public ToggleVisibilityCommand(WindowViewModel viewModel)
+    {
+        _viewModel = viewModel;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        // Toggle button visibility
+        _viewModel.ButtonVisibility = _viewModel.ButtonVisibility switch
+        {
+            Visibility.Hidden => Visibility.Visible,
+            Visibility.Visible => Visibility.Hidden,
+            _ => Visibility.Hidden
+        };
+    }
+}
+
+internal class OpenWindowCommand : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        var window = new MainWindow();
+        window.Show();
+    }
+}
